f58852b7e6d4d011e8b154eaf576788f2d182b5f
Amir Drabo <draboamir01@gmail.com>
2023-06-30 10:02:20 +0000

    Update 2023_06_29_140652_create_users_table.php

--- database/migrations/2023_06_29_140652_create_users_table.php
+++ database/migrations/2023_06_29_140652_create_users_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('first_name');
+            $table->string('user_phone');
+            $table->string('email')->unique();
+            $table->string('username');
+            $table->string('password');
+            $table->string('role');
             $table->timestamps();
         });
     }
